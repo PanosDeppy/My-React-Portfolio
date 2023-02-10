572b2fb4f9306e19b8b8f07906f8e98d1713a27b
PanosDeppy <panagiotiswb@gmail.com>
2023-02-10 15:38:39 +0000

    update contact details

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -8,14 +8,14 @@ export const Socials = () => {
       <hr className="hr" />
 
       <br></br>
-      <div className="d-flex d-flex justify-content-evenly fs-4">
+      <div className="d-flex d-flex justify-content-center fs-4">
         <a
           href="https://www.linkedin.com/in/panagiotis-ioannidis-panos/"
           target="_blank"
           rel="noreferrer"
           className="contact-icon"
         >
-          <i className="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-linkedin px-4"></i>
         </a>
 
         <a
@@ -24,25 +24,7 @@ export const Socials = () => {
           rel="noreferrer"
           className="contact-icon"
         >
-          <i className="git-hub-icon fa-brands fa-github"></i>
-        </a>
-
-        <a
-          href="https://mail.google.com/mail/"
-          target="_blank"
-          rel="noreferrer"
-          className="contact-icon"
-        >
-          <i className="fa-solid fa-envelope"></i>
-        </a>
-
-        <a
-          href="tel:+447386109901"
-          target="_blank"
-          rel="noreferrer"
-          className="contact-icon"
-        >
-          <i className="fa-solid fa-phone"></i>
+          <i className="git-hub-icon fa-brands fa-github px-4"></i>
         </a>
       </div>
       <div className="designed fs-5 pt-4 pb-3 d-flex justify-content-center">
